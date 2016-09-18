a6a0b09cc6116996029e2e9d1afbe5d6a257c825
yikouniao <chinoandtippi@gmail.com>
2016-09-18 13:10:02 +0800

    Test opencv 3.1

---
+++ main.cpp
@@ -0,0 +1,33 @@
+/*************************************************************************
+	> File Name: main.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 2016年09月18日 星期日 12时43分45秒
+ ************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+#include <opencv2/opencv.hpp>
+using namespace cv;
+
+int main(int argc, char** argv) {
+  if (argc != 2) {
+    cout << "usage: DisplayImage.out <Image_Path>\n";
+    return -1;
+  }
+
+  Mat img = imread(argv[1], 1);
+
+  if (!img.data) {
+    cout << "No image date.\n";
+    return -1;
+  }
+
+  namedWindow("Display Image");
+  imshow("Display Image", img);
+
+  waitKey(0);
+
+  return 0;
+}
